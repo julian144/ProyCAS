ad78d8ca77f8b3812c3594b7239b9528f620bcf0
julian144 <julian.daniel1995@gmail.com>
2018-08-05 18:35:15 -0700

    Update Config.php
    Change the pass

--- includes/config.php
+++ includes/config.php
@@ -14,7 +14,7 @@
 */
   define( 'DB_HOST', 'localhost' );          // Set database host
   define( 'DB_USER', 'root' );             // Set database user
-  define( 'DB_PASS', '' );             // Set database password
+  define( 'DB_PASS', 'lokos05' );             // Set database password
   define( 'DB_NAME', 'oswa_inv' );        // Set database name
 
 ?>
